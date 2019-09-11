4388010a7e65ae6e2b7e13324a995d826cb041ee
Zoltan Toth <soldyx@gmail.com>
2019-09-11 10:15:17 +0200

    Add heroku

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const fileupload = require('express-fileupload');
-const connectDB = require('./config/db');
+const express = require("express");
+const fileupload = require("express-fileupload");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 // Connect Database
 connectDB();
 
-
 // Init middlewears
 app.use(fileupload());
-app.use(express.json({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb", extended: true }));
 
 // Enable upload folder by making it static
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
+//app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+// Server static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
